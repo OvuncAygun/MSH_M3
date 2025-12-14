3c95dafb204b51cfc94c05ad5b46be17d3cc2d9c
Nigara Suleimanova <nigaraazamatovna@gmail.com>
2025-12-14 16:29:25 +0300

    Create BaseStrategy with toggleDevices helper

---
+++ include/ModeStrategies.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "IModeStrategy.h"
+#include "../external/IDeviceManager.h"
+#include "../external/DeviceType.h"
+#include "../external/IDevice.h"
+#include <vector>
+#include <iostream>
+#include <string>
+
+class BaseStrategy : public IModeStrategy {
+protected:
+    void toggleDevices(IDeviceManager* dm, DeviceType type, bool turnOn) {
+        std::vector<IDevice*> devices = dm->getDeviceByType(type);
+        for (size_t i = 0; i < devices.size(); ++i) {
+            if (devices[i]) {
+                if (turnOn) devices[i]->poweron();
+                else devices[i]->poweroff();
+            }
+        }
+    }
+};
