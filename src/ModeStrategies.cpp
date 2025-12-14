43c94f4364f633062c0a10695928e2e92ec57d66
Nigara Suleimanova <nigaraazamatovna@gmail.com>
2025-12-14 17:03:54 +0300

    Implement EveningStrategy configuration logic

--- src/ModeStrategies.cpp
+++ src/ModeStrategies.cpp
@@ -7,3 +7,13 @@ void NormalStrategy::configureDevices(IDeviceManager* dm) {
     this->toggleDevices(dm, Light, true);
     this->toggleDevices(dm, Music, false);
 }
+
+
+
+void EveningStrategy::configureDevices(IDeviceManager* dm) {
+    
+    this->toggleDevices(dm, Light, false);
+    this->toggleDevices(dm, Music, false);
+    
+    this->toggleDevices(dm, TV, true); 
+}
