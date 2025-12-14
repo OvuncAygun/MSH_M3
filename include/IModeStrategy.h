ac42b659f44dd8ce807809d51d950cf90a06fbee
Nigara Suleimanova <nigaraazamatovna@gmail.com>
2025-12-14 15:41:00 +0300

    Fix: Added missing configureDevices method to interface

--- include/IModeStrategy.h
+++ include/IModeStrategy.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <string>
-
+#include "../external/IDeviceManager.h"
 class IModeStrategy {
 public:
     virtual ~IModeStrategy() = default;
+
+    virtual void configureDevices(IDeviceManager* dm) = 0;
 
     virtual std::string getModeName() const = 0;
 };
