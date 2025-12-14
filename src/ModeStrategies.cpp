9f072bc21da787f64fa89614fd17e89b7d050201
Nigara Suleimanova <nigaraazamatovna@gmail.com>
2025-12-14 17:04:39 +0300

    Implement PartyStrategy configuration logic

--- src/ModeStrategies.cpp
+++ src/ModeStrategies.cpp
@@ -17,3 +17,12 @@ void EveningStrategy::configureDevices(IDeviceManager* dm) {
     
     this->toggleDevices(dm, TV, true); 
 }
+
+
+
+void PartyStrategy::configureDevices(IDeviceManager* dm) {
+    
+    this->toggleDevices(dm, Light, true);
+    this->toggleDevices(dm, Music, true);
+    this->toggleDevices(dm, TV, false); 
+}
